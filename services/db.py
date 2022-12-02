8660d427ce13a6b039df57f9d735cb83892d76d4
Ramón Loaiza Chávez <ramon.loaiza17@tectijuana.edu.mx>
2022-12-02 04:04:08 -0800

    [Fix - Port Mysql]

--- services/db.py
+++ services/db.py
@@ -12,7 +12,7 @@ load_dotenv() # Take environment variables from .env
 
 # MySQL - Settings
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'localhost'
-app.config['MYSQL_POST'] = os.getenv('MYSQL_POST') or 3306
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT')) or 3306
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or None
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or None
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or None
